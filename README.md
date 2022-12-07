f5c0dd4af79fb077a34fe5795a92a94662d66808
den-taku <dentaku1412@gmail.com>
2022-12-07 19:35:12 +0900

    Get address

---
+++ README.md
@@ -0,0 +1,3 @@
+# test-socket
+
+sample code for socket communication with `std::net` in Rust
--- src/bin/server.rs
+++ src/bin/server.rs
@@ -1,11 +1,21 @@
 use std::net::ToSocketAddrs;
 
-fn main() {
-    // host name
+fn main() -> std::io::Result<()> {
+    // host name (maybe 127.0.0.1)
     let host = "localhost";
     // port
     let port = 3456;
+
     // generate SocketAddr (ref: https://doc.rust-lang.org/std/net/trait.ToSocketAddrs.html)
-    let addrs_iter = (host, port).to_socket_addrs();
-    println!("{:?}", addrs_iter);
+    let mut addrs_iter = (host, port).to_socket_addrs()?;
+    let addr_ipv6 = addrs_iter
+        .find(|addr| addr.is_ipv6())
+        .expect("failed to get ipv6 address");
+    println!("ipv6 address is {addr_ipv6:?}.");
+    let addr_ipv4 = addrs_iter
+        .find(|addr| addr.is_ipv4())
+        .expect("failed to get ipv4 address");
+    println!("ipv4 address is {addr_ipv4:?}.");
+
+    Ok(())
 }
